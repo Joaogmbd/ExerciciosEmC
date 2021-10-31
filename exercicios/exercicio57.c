5f5a8bc6de0af5d11351a7ad876438e7dd8c50ef
João Gabriel Moreira Bossatto Daré <79703333+Joaogmbd@users.noreply.github.com>
2021-10-31 16:08:47 -0300

    Update exercicio57.c
    Aplicado o enunciado do exercício

--- exercicios/exercicio57.c
+++ exercicios/exercicio57.c
@@ -1,3 +1,17 @@
+/* 
+57) Dadas duas matrizes de inteiros A e B de ordem 4 x 3, fazer um programa C que gere uma matriz de 
+zeros e uns denominada C, tal que o elemento C[i, j] seja verdadeiro (1 na linguagem C) se os elementos 
+nas posições respectivas das matrizes A e B forem múltiplos e falso (0 na linguagem C), caso contrário. 
+Exibir na tela a matriz C. As matrizes A e B recebem valores inteiros aleatórios, obrigatoriamente pares, 
+no intervalo entre [2, 100].
+
+A = 30  100 22      B = 6   10  10      C = 1   1   0
+    50  8   66          80  50  22          0   0   1
+    28  4   2           14  30  40          1   0   0
+    40  20  90          100 20  30          0   1   1
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
